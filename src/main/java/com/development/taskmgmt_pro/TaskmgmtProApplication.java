745a82dcdeffac8911ddd54444b2c2312b3f83a1
santhosh-arumugam <santhoshkarumugam94@gmail.com>
2025-04-05 12:21:36 -0400

    Initial Template

---
+++ src/main/java/com/development/taskmgmt_pro/TaskmgmtProApplication.java
@@ -0,0 +1,13 @@
+package com.development.taskmgmt_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmgmtProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmgmtProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/taskmgmt_pro/TaskmgmtProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.taskmgmt_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmgmtProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
